d3458e6afd2515a74508d2c010276bbd686acd88
Aashish Pathak <aashishgnubca@gmail.com>
2022-04-16 20:59:40 +0530

    Initial

---
+++ src/main/java/Utils/Global.java
@@ -0,0 +1,12 @@
+package Utils;
+
+public class Global {
+    public static String GrpName = "Test_105";
+    public static String LaunchURL = "";
+    public static String AdminUserName = "";
+    public static String AdminPassword = "";
+    public static String TPToken = "zjFeUDW1WWXm7YZRggfMO-_l6tEljdK3pxucdvHaaU4";
+    public static String TPUrl = "http://localhost:8585";
+    public static int THINK = 1;
+    public static final String DEALERSHIPS_SPREADSHEET = "";
+}
